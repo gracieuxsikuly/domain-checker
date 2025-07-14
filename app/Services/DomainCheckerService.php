ffb8790123569c757ba685c213e7ac1adc5566a9
gracieuxsikuly <graciersikuly@gmail.com>
2025-07-14 10:34:35 +0200

    goodgood

--- app/Services/DomainCheckerService.php
+++ app/Services/DomainCheckerService.php
@@ -16,12 +16,6 @@ class DomainCheckerService
     {
         $this->whois = Factory::get()->createWhois();
     }
-    // public function __construct()
-    // {
-    //     // Mets ta vraie clé ici
-    //     $this->apiKey ="at_LXhHecolDmeWI128AOITbHX3T3Cto";
-    //     //  env('WHOISXML_API_KEY');
-    // }
 
      public function checkDomain($domain)
     {
@@ -39,7 +33,6 @@ class DomainCheckerService
             'available' => false,
             'registered_at' => $info->getCreationDate() // retourne un timestamp
         ];
-        // return $this->whois->loadDomainInfo($domain) === null;
     } catch (WhoisException $e) {
         // Exception liée au WHOIS
        throw new \Exception("Cette extension de domaine n'est pas prise en charge. Veuillez essayer avec une autre comme .com, .net, .org, etc.");
@@ -47,55 +40,12 @@ class DomainCheckerService
     } catch (\Throwable $e) {
         // Toutes autres exceptions (ex : erreur réseau, mauvais format, etc.)
        throw new \Exception("Problème technique lors de la vérification. Veuillez réessayer dans quelques instants ou changer de domaine.");
-
     }
-        // // On crée une clé unique pour ce domaine
-        // $cacheKey = 'domain_check_' . $domain;
-
-        // // Si le résultat est déjà en cache → on le retourne directement
-        // if (Cache::has($cacheKey)) {
-        //     return Cache::get($cacheKey);
-        // }
-        // try {
-        //     // Requête API WHOISXML
-        //     $response = Http::timeout(5)->get('https://www.whoisxmlapi.com/whoisserver/WhoisService', [
-        //         'domainName' => $domain,
-        //         'apiKey' => $this->apiKey,
-        //     ]);
-        //     $data = $response->json();
-        //     // On vérifie si le domaine est disponible
-        //     $available = isset($data['WhoisRecord']['dataError']) && $data['WhoisRecord']['dataError'] === 'MISSING_WHOIS_DATA';
-
-        //     // On stocke le résultat dans le cache pour 10 minutes (600 secondes)
-        //     Cache::put($cacheKey, $available, now()->addMinutes(10));
-
-        //     return $available;
-        // } catch (\Exception $e) {
-        //     return false;
-        // }
     }
-
-//    public function checkDomain($domain)
-// {
-//     try {
-//         $response = Http::timeout(10)->get('https://www.whoisxmlapi.com/whoisserver/WhoisService', [
-//             'domainName' => $domain,
-//             'apiKey' => $this->apiKey,
-//         ]);
-
-//         $data = $response->json();
-
-//         // Analyse basique du contenu (tu peux améliorer si besoin)
-//         return isset($data['WhoisRecord']['dataError']) && $data['WhoisRecord']['dataError'] === 'MISSING_WHOIS_DATA';
-//     } catch (\Exception $e) {
-//         return false;
-//     }
-// }
-
     public function checkAll($name, $excludedExt = null)
     {
         $results = [];
-       $extensions = Extension::limit(10)->get();
+       $extensions = Extension::all();
       foreach ($extensions as $ext) {
         if ($excludedExt && strtolower($ext->extension) === strtolower($excludedExt)) {
             continue; // Ignore l'extension déjà saisie
@@ -105,7 +55,7 @@ class DomainCheckerService
 
         $results[] = [
             'domain' => $domain,
-            'available' => $available,
+            'available' => $available['available'], 
             'price' => $ext->prix,
             'old_price' => $ext->old_price,
             'currency' => $ext->currency,
